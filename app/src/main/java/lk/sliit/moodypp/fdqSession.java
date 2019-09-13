c5636621bcb73b8948f9f0c7fb6040669ba4d890
ruudiana <rashinigangadari95@gmail.com>
2019-09-13 18:52:02 +0530

    chatbot merged to sra

---
+++ app/src/main/java/lk/sliit/moodypp/fdqSession.java
@@ -0,0 +1,49 @@
+package lk.sliit.moodypp;
+
+public class fdqSession {
+    private double fdq1;
+    private double fdq2;
+    private double fdq3;
+    private double fdq4;
+    private double fdq5;
+
+    public double getFdq1() {
+        return fdq1;
+    }
+
+    public double getFdq2() {
+        return fdq2;
+    }
+
+    public double getFdq3() {
+        return fdq3;
+    }
+
+    public double getFdq4() {
+        return fdq4;
+    }
+
+    public double getFdq5() {
+        return fdq5;
+    }
+
+    public void setFdq1(double fdq1) {
+        this.fdq1 = fdq1;
+    }
+
+    public void setFdq2(double fdq2) {
+        this.fdq2 = fdq2;
+    }
+
+    public void setFdq3(double fdq3) {
+        this.fdq3 = fdq3;
+    }
+
+    public void setFdq4(double fdq4) {
+        this.fdq4 = fdq4;
+    }
+
+    public void setFdq5(double fdq5) {
+        this.fdq5 = fdq5;
+    }
+}
